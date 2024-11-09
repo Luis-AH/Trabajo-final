eabad1cd78d1397731c95fb5318bcb2b0cb8dcfd
Luis-AH <luis_asin@usmp.pe>
2024-11-08 23:56:05 -0500

    version 8.6

--- JS/script_shop.js
+++ JS/script_shop.js
@@ -1,4 +1,13 @@
-const Card = ({ category, srcimagen, title, description, price, id }) => {
+const Card = ({
+  category,
+  srcimagen,
+  title,
+  description,
+  price,
+  id,
+  type,
+  age,
+}) => {
   return (
     <div className="pet text-center justify-center rounded-md mx-auto bg-white max-w-2xl px-2 py-5 sm:px-6 sm:py-20 lg:max-w-7xl lg:px-8">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
@@ -24,50 +33,83 @@ const CardList = ({ cardsData }) => {
     {cardsData.map((card, index) => (
       <Card
         key={index}
+        id={card.id}
         category={card.category}
         srcimagen={card.src}
         title={card.title}
         description={card.description}
         price={card.price}
+        type={card.type}
+        age={card.age}
       />
     ))}
   </div>;
   const [selectedCategory, setSelectedCategory] = React.useState("");
+  const [selectedType, setSelectedType] = React.useState("");
+
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
   };
-  const filteredCards = selectedCategory
-    ? cardsData.filter((card) => card.category === selectedCategory)
-    : cardsData;
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+  };
+  const filteredCards = cardsData.filter((card) => {
+    const matchesCategory = selectedCategory
+      ? card.category === selectedCategory
+      : true;
+    const matchesType = selectedType ? card.type === selectedType : true;
+
+    return matchesCategory && matchesType;
+  });
 
   return (
-    <div>
+    <div className="filter-container">
       {/* Selector de categoría */}
-      <div className="mb-5 col-span-1 lg:row-span-2">
-        <label htmlFor="category-filter">Filtrar por categoría:</label>
-        <select
-          id="category-filter"
-          onChange={handleCategoryChange}
-          value={selectedCategory}
-        >
-          <option value="">Tipo de mascota</option>
-          <option value="perro">Perro</option>
-          <option value="gato">Gato</option>
-          <option value="masPequeños">Macotas pequeñas</option>
-          {/* Añade más opciones aquí según tus categorías */}
-        </select>
+      <div className="grid">
+        <div className="category-filter grid">
+          <label htmlFor="category-filter">Filtrar por categoría:</label>
+          <select
+            id="category-filter"
+            onChange={handleCategoryChange}
+            value={selectedCategory}
+          >
+            <option value="">Tipo de mascota</option>
+            <option value="perro">Perro</option>
+            <option value="gato">Gato</option>
+            <option value="masPequeños">Mascotas pequeñas</option>
+            {/* Añade más opciones aquí según tus categorías */}
+          </select>
+        </div>
+
+        <div className="category-filter">
+          <select
+            id="type-filter"
+            onChange={handleTypeChange}
+            value={selectedType}
+          >
+            <option value="">Tipo de producto</option>
+            <option value="alimento">Alimentos</option>
+            <option value="salud">Salud e higiene</option>
+            <option value="casa">Casas</option>
+            <option value="accesorio">Accesorios y otros</option>
+            {/* Añade más opciones aquí según tus categorías */}
+          </select>
+        </div>
       </div>
 
       {/* Renderizado de tarjetas filtradas */}
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 xl:gap-x-8 min-[320px]:text-center max-[600px]:bg-sky-300 flex-1">
+      <div className="cards-grid grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 xl:gap-x-8 min-[320px]:text-center max-[600px]:bg-sky-300">
         {filteredCards.map((card, index) => (
           <Card
             key={index}
+            id={card.id}
             category={card.category}
             srcimagen={card.src}
             title={card.title}
             description={card.description}
             price={card.price}
+            type={card.type}
+            age={card.age}
           />
         ))}
       </div>
